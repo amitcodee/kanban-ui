10eaac428e47e8232644885f50b317ca140c3ae9
Amit Sharma <pandit143amit@gmail.com>
2024-05-01 15:05:33 +0530

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
   {
@@ -13,4 +14,7 @@ export const routes: Routes = [
     path: 'sign-up',
     component: SignUpComponent,
   },
+  {
+    path: 'dashboard', component: DashboardComponent
+  }
 ];
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
+})
+export class DashboardComponent {
+$dispatch(arg0: string) {
+throw new Error('Method not implemented.');
+}
+
+}
